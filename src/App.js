c3e85e05a8069158d0b10e653d91e1cb56db2b3a
Shrill Shrestha <shrill.shrestha@gmail.com>
2021-09-22 14:32:10 -0500

    Add 'create category' page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-import './App.css';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import CreateCategory from './components/pages/CreateCategory/CreateCategory';
+import NavComponent from './components/elements/NavComponent/NavComponent';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
+import {Route, Switch } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="App">
-      <AmplifySignOut />
-      <h1>Personal Diary</h1>
-    </div>
+    <>
+      <NavComponent />
+      <Switch>
+        <Route exact={true} path='/'>
+          <h1>Personal Diary</h1>
+        </Route>
+        <Route exact={true} path='/categories/create'>
+          <CreateCategory />
+        </Route>
+      </Switch>
+    </>
   );
 }
 
---
+++ src/components/elements/NavComponent/NavComponent.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import {Navbar, Container, Nav} from 'react-bootstrap';
+import styles from './NavComponent.module.css';
+import { AmplifySignOut } from '@aws-amplify/ui-react';
+
+const NavComponent = () => {
+  return (
+    <Navbar className={styles.nav_main} bg="light" variant="light">
+      <Container fluid className={styles.nav_pad}>
+        <Navbar.Brand href="/">
+          <img
+            alt="Company logo"
+            src="/images/logo.png"
+            width="30"
+            height="30"
+            className="d-inline-block align-top"
+          />{' '}
+          Digital Diary
+        </Navbar.Brand>
+        <Nav className={`d-flex ${styles.nav_look}`}>
+          <Nav.Link href="/">Home</Nav.Link>
+          <Nav.Link href="/categories">Categories</Nav.Link>
+          <Nav.Link href="/about">About</Nav.Link>
+          <AmplifySignOut />
+        </Nav>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default NavComponent;
