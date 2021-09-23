41f03314a477c33e4d1ae68bdc2d0f69fc8583f3
Kirsten <korjnan@gmail.com>
2021-09-23 22:29:55 +0800

    Add entry list page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
-import CreateCategory from './components/pages/CreateCategory/CreateCategory';
-import NavComponent from './components/elements/NavComponent/NavComponent';
 import { withAuthenticator } from '@aws-amplify/ui-react';
-
 import {Route, Switch } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import NavComponent from './components/elements/NavComponent/NavComponent';
+import CreateCategory from './components/pages/CreateCategory/CreateCategory';
 import EntryNew from './components/pages/CreateEntry/EntryNew';
+import EntryList from './components/pages/EntryList/EntryList';
+
+
 
 function App() {
   return (
@@ -16,6 +18,9 @@ function App() {
         </Route>
         <Route exact={true} path='/categories/create'>
           <CreateCategory />
+        </Route>
+        <Route exact path='/entries'>
+          <EntryList />
         </Route>
         <Route exact path='/entries/create'>
           <EntryNew />
---
+++ src/components/pages/EntryList/EntryList.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { Card, Button } from "react-bootstrap";
+import styles from "./EntryList.module.css";
+
+const EntryList = () => {
+  return (
+    <div className={styles.main_div}>
+      <div className={styles.sub_div}>
+        <div className={styles.title_div}>Entries</div>
+        {[0, 1, 2, 3, 4].map((i) => {
+          return (
+            <Card className={styles.entry_card}>
+              <Card.Body>
+                <Card.Title>Entry title</Card.Title>
+                <Card.Text>
+                  With supporting text below as a natural lead-in to additional
+                  content.
+                </Card.Text>
+                <Button variant="primary">View entry</Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
+export default EntryList;
