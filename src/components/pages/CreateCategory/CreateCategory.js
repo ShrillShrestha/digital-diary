53ad725ba49d26bbe3016a15781b9899f6dacf2f
Kirsten <korjnan@gmail.com>
2021-09-24 23:48:16 +0800

    Update UI

--- src/components/pages/CreateCategory/CreateCategory.js
+++ src/components/pages/CreateCategory/CreateCategory.js
@@ -1,58 +1,81 @@
-import React, { useState } from 'react';
-import {Form, Button} from 'react-bootstrap';
-import styles from './CreateCategory.module.css';
-import { API, graphqlOperation } from 'aws-amplify'
-import { createCategory } from '../../../graphql/mutations';
+import React, { useState } from "react";
+import { Form, Button } from "react-bootstrap";
+import styles from "./CreateCategory.module.css";
+import { API, graphqlOperation } from "aws-amplify";
+import { createCategory } from "../../../graphql/mutations";
 import { useHistory } from "react-router-dom";
 
-
 const CreateCategory = () => {
-
   const initialFormData = {
-    name: '',
-    imageUrl: '',
-    description: ''
-  }
+    name: "",
+    imageUrl: "",
+    description: "",
+  };
 
-  const [category, setCategory] = useState(initialFormData)
+  const [category, setCategory] = useState(initialFormData);
   let history = useHistory();
 
   const onSubmit = async (event) => {
     event.preventDefault();
-    console.log(category)
+    console.log(category);
     try {
-      await API.graphql(graphqlOperation(createCategory, {
-        input: category
-      }))
+      await API.graphql(
+        graphqlOperation(createCategory, {
+          input: category,
+        })
+      );
       history.push("/categories");
-    } catch(error) {
-      console.log(error)
+    } catch (error) {
+      console.log(error);
     }
+  };
+
+  const cancelAdd = () => {
+    history.push("/categories")
   }
 
   return (
     <div className={styles.main_div}>
       <div className={styles.sub_div}>
-      <div className={styles.title_div}>
-          Create a New Category
-      </div>
-      <Form className={styles.cat_form} onSubmit={onSubmit}>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Category Name</Form.Label>
-          <Form.Control type="text" placeholder="E.g. Travel" onChange={(event) => setCategory({...category, name: event.target.value})}/>
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
-          <Form.Label>Category Image URL</Form.Label>
-          <Form.Control type="text" placeholder="https://imageyouwant.com/img1.jpg" onChange={(event) => setCategory({...category, imageUrl: event.target.value})} />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
-          <Form.Label>Category Description</Form.Label>
-          <Form.Control as="textarea" rows={7} onChange={(event) => setCategory({...category, description: event.target.value})}/>
-        </Form.Group>
-        <Button variant="info" type="submit">
-          Submit
-        </Button>
-      </Form>
+        <div className={styles.title_div}>Create a New Category</div>
+        <Form className={styles.cat_form} onSubmit={onSubmit}>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Category Name</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="E.g. Travel"
+              onChange={(event) =>
+                setCategory({ ...category, name: event.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+            <Form.Label>Category Image URL</Form.Label>
+            <Form.Control
+              type="text"
+              placeholder="https://imageyouwant.com/img1.jpg"
+              onChange={(event) =>
+                setCategory({ ...category, imageUrl: event.target.value })
+              }
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+            <Form.Label>Category Description</Form.Label>
+            <Form.Control
+              as="textarea"
+              rows={7}
+              onChange={(event) =>
+                setCategory({ ...category, description: event.target.value })
+              }
+            />
+          </Form.Group>
+          <div className={styles.btn_div}>
+            <Button variant="danger" onClick={cancelAdd}>Cancel</Button>
+            <Button variant="info" type="submit">
+              Submit
+            </Button>
+          </div>
+        </Form>
       </div>
     </div>
   );
--- src/components/pages/HomePage/HomePage.js
+++ src/components/pages/HomePage/HomePage.js
@@ -6,11 +6,17 @@ const HomePage = () => {
   return (
     <div className={styles.main_div}>
       <div className={styles.sub_div}>
-        <div className={styles.title_div}>
-          Welcome to Digital Diary!
+        <div className={styles.title_div}>Welcome to Digital Diary!</div>
+        <div className={styles.landing_div}>
+          <img
+            className={styles.landing_img}
+            src="https://64.media.tumblr.com/8ede0a31deaf6c972d874a6aa3caaf35/tumblr_my8p5nLuJ11rov8qno1_400.gifv"
+            alt="App logo"
+          />
+          <Link to="/categories" className="btn btn-primary">
+            Sign In/ Sign Up
+          </Link>
         </div>
-        <img src='https://64.media.tumblr.com/8ede0a31deaf6c972d874a6aa3caaf35/tumblr_my8p5nLuJ11rov8qno1_400.gifv' alt="App logo" />
-        <Link to= "/categories">Sign In/ Sign Up</Link>
       </div>
     </div>
   );
