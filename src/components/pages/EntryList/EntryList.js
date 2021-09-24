19ac255988d7a0fd5cb68076438ba32096fc5df5
Kirsten <korjnan@gmail.com>
2021-09-24 21:25:02 +0800

    UI and functionality fixes

--- src/components/pages/EntryList/EntryList.js
+++ src/components/pages/EntryList/EntryList.js
@@ -4,7 +4,10 @@ import styles from "./EntryList.module.css";
 import { API } from "aws-amplify";
 import { entriesByCategory } from "../../../graphql/queries";
 import { Link, useParams } from "react-router-dom";
-import { Auth } from 'aws-amplify';
+import { Auth } from "aws-amplify";
+import { BiEdit } from 'react-icons/bi';
+import { MdDelete } from 'react-icons/md';
+import * as mutations from '../../../graphql/mutations';
 
 const EntryList = () => {
   const { id } = useParams();
@@ -13,7 +16,7 @@ const EntryList = () => {
 
   const fetchEntriesByCategory = async () => {
     let user = await Auth.currentAuthenticatedUser();
-    console.log(user.username)
+    console.log(user.username);
     try {
       const entriesData = await API.graphql({
         query: entriesByCategory,
@@ -30,35 +33,61 @@ const EntryList = () => {
   };
 
   useEffect(() => {
-    fetchEntriesByCategory();   
+    fetchEntriesByCategory();
   }, []);
+
+  const deleteEntry = async (entryID) => {
+    console.log('entryID',entryID)
+    const entryDetails = {
+      id: entryID
+    };
+
+    await API.graphql({
+      query: mutations.deleteEntry,
+      variables: { input: entryDetails },
+    });
+  };
+
 
   return (
     <div className={styles.main_div}>
       <div className={styles.sub_div}>
         <div className={styles.title_div}>
           <span>Entries </span>
-          <Link to={`/${id}/entries/create`}>Create a new Entry</Link>
+          <Link to={`/${id}/entries/create`} className="btn btn-primary">
+            Create a new Entry
+          </Link>
         </div>
         <div className={styles.card_div}>
-          {entries.length !== 0 ? 
+          {entries.length !== 0 ? (
             entries.map((entry) => {
               return (
                 <Card className={styles.entry_card} key={entry.id}>
-                <Card.Body>
-                  <Card.Title>{entry.title}</Card.Title>
-                  <Card.Text className={styles.truncate}>
-                    {entry.content}
-                  </Card.Text>
-                  {/* <Link to={`/${id}/entries/${entry.id}`}>View Entries</Link> */}
-                  <Link to={{pathname:`/${id}/entries/${entry.id}`, state: {entry} }}>View Entry</Link>
-                </Card.Body>
-              </Card>
-              )
-            }) : (
-              <h1 style={{ color: "#fff" }}>No entries, create</h1>
-            )
-          } 
+                  <Card.Body>
+                    <Card.Title>{entry.title}</Card.Title>
+                    <Card.Text className={styles.truncate}>
+                      {entry.content}
+                    </Card.Text>
+                    <Link
+                      to={{
+                        pathname: `/${id}/entries/${entry.id}`,
+                        state: { entry },
+                      }}
+                      className="btn btn-primary"
+                    >
+                      View Entry
+                    </Link>
+                    <div className={styles.footer_icons}>
+                      <BiEdit color="green" size="25px" className={styles.edit_icon}/>
+                      <MdDelete color="red" size="25px" onClick={() => {deleteEntry(entry.id)}}/>
+                    </div>
+                  </Card.Body>
+                </Card>
+              );
+            })
+          ) : (
+            <h1 style={{ color: "#fff" }}>No entries, create</h1>
+          )}
         </div>
       </div>
     </div>
